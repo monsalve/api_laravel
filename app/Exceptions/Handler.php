4f2ab639a8c7c1810d53c8c61b4bb8415ebe0ceb
Cristhiam Monsalve <ccmonapa@hotmail.com>
2020-05-12 03:04:32 -0500

    listo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+use Illuminate\Database\QueryException;
+
 use Throwable;
 
 class Handler extends ExceptionHandler
